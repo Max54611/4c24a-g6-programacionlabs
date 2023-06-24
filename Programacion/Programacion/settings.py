f97451e72f64146d264db50abd87785203b1f4dd
LuisCorrales <luis.corrales@tecsup.edu.pe>
2023-06-24 15:34:15 -0500

    mysql

--- Programacion/Programacion/settings.py
+++ Programacion/Programacion/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'programacion_laboratorios',
         'USER':'root',
-        'PASSWORD':'',
+        'PASSWORD':'12345',
         'HOST':'localhost',
         'PORT':'3306',
     }
