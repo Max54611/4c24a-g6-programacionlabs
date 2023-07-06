002205f21596fac75670933118dcd3424a6b4da8
LuisCorrales <luis.corrales@tecsup.edu.pe>
2023-07-06 00:38:01 -0500

    Login

--- Programacion/Laboratorios/urls.py
+++ Programacion/Laboratorios/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from .import views
 
-urlpatterns = [
+app_name = 'Laboratorios'
 
+urlpatterns = [
+    path('',views.index, name='index'),
     path('listado',views.progLab, name='labs'),
-    path('registrar',views.registrar),
-    path('editar/<id>',views.editar_datos),
-    path('editar',views.editar),
-    path('eliminar/<id>',views.eliminar)
+    path('registrar',views.registrar, name='registrar'),
+    path('editar/<id>',views.editar_datos, name='editar'),
+    path('editar',views.editar, name='actualizar'),
+    path('eliminar/<id>',views.eliminar, name='eliminar'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout_view, name='logout_view'),
 ]
--- Programacion/Laboratorios/views.py
+++ Programacion/Laboratorios/views.py
@@ -1,21 +1,68 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout
+from django.utils.translation import gettext_lazy as _
 from django.db.models import Q
-from django.db import IntegrityError
 from django.contrib import messages
 
 # Create your views here.
 
+class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = 'El nombre de usuario debe ser menor a 150 caracteres y no puede contener caracteres especiales'
+        self.fields['password1'].help_text = 'La contraseña debe tener al menos 3 caracteres'
+        self.fields['password2'].help_text = 'La contraseña debe tener al menos 3 caracteres'
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('Laboratorios:login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'autenticacion/register.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('/listado')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'autenticacion/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
 
 #PROGRAMACION LABORATORIOS
+
+def index(request):
+    listadoProgLabs = Programacion_laboratorio.objects.all()
+    laboratorios = Laboratorio.objects.all()
+    cursos_dictados = Curso_dictado.objects.all()
+    context = {
+        "labs": listadoProgLabs,
+        "laboratorios": laboratorios,
+        "cursos_dictados": cursos_dictados
+    }
+    return render(request, "index.html", context)
+
+@login_required(login_url='Laboratorios:login')
 def progLab(request):
     listadoProgLabs = Programacion_laboratorio.objects.all()
     laboratorios = Laboratorio.objects.all()
     cursos_dictados = Curso_dictado.objects.all()
     return render(request, "listado.html", {"labs": listadoProgLabs, "laboratorios": laboratorios, "cursos_dictados": cursos_dictados})
 
-#No se permite registrar un laboratorio en una fecha y hora que ya se encuentre registrado o en el mismo rango de horas
+@login_required(login_url='Laboratorios:login')
 def registrar(request):
     laboratorio_id = request.POST["txtlaboratorio"]
     curso_dictado_id = request.POST["txtcurso_dictado"]
@@ -41,6 +88,7 @@ def registrar(request):
         messages.success(request, "Programación de laboratorio registrada correctamente.")
     return redirect('/listado')
 
+@login_required(login_url='Laboratorios:login')
 def editar_datos(request,id):
     datos=Programacion_laboratorio.objects.get(id=id)
 
@@ -51,16 +99,24 @@ def editar_datos(request,id):
     codigo_curso=datos.curso_dictado.id
     curso_dictado=Curso_dictado.objects.get(pk=codigo_curso)
     cursoListado=Curso_dictado.objects.exclude(pk=codigo_curso)
+    
+    listadoProgLabs = Programacion_laboratorio.objects.all()
+    laboratorios = Laboratorio.objects.all()
+    cursos_dictados = Curso_dictado.objects.all()
 
     context={
         'datos':datos,
         'laboratorio':laboratorio,
         'laboratorios':laboratorioListado,
         'curso_dictado':curso_dictado,
-        'cursos':cursoListado
+        'cursos':cursoListado,
+        "labs": listadoProgLabs,
+        "laboratorios": laboratorios,
+        "cursos_dictados": cursos_dictados
     }
     return render(request,"editar.html",context)
 
+@login_required(login_url='Laboratorios:login')
 def editar(request):
     id=request.POST["idpro"]
     laboratorio_id=request.POST["txtlaboratorio"]
@@ -88,6 +144,7 @@ def editar(request):
     
     return redirect('/listado')
 
+@login_required(login_url='Laboratorios:login')
 def eliminar(request,id):
     program=Programacion_laboratorio.objects.get(id=id)
     program.delete()
